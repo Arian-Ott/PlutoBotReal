119d7ce4add3b746260c0d3b89eede6b57c8347f
ottar <arian.ott@thunderlight.de>
2022-11-23 22:24:40 +0100

    v 1.0.1 First update
    MyListener added

--- src/main/java/main/Bot.java
+++ src/main/java/main/Bot.java
@@ -3,16 +3,18 @@ package main;
 import configurations.Config;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class Bot {
+public class Bot extends ListenerAdapter {
 
-public Bot() {
-    
-}
+
         public static void main(String[] arguments) throws Exception
         {
             Config config = new Config();
 
             JDA api = JDABuilder.createDefault(config.getToken()).build();
+            JDA.addEventListeners(new MyListener());
+
+
         }
     }
